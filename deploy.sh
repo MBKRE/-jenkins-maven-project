b262e0f55bd3752ad03a05a3d05736349ad4b2d2
MBKRE <112285053+MBKRE@users.noreply.github.com>
2023-02-05 23:55:02 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export public_ip=`terraform output "public_ip"|tr -d '"'`
-scp -i ${SSH_KEY_UBUNTU} ./target/hello.war ubuntu@$public_ip:/root/webapps
+scp -i ${SSH_KEY_UBUNTU} ./target/hello.war ubuntu@$public_ip:/tmp
+ssh i -c "sudo cp /tmp /root/apache-tomcat-9.0.71/webapps"
