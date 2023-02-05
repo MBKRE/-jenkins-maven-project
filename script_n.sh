42e413d33e48f832b29b672902dc208ed2a5d756
reddyb114@gmail.com <root@ip-172-31-53-25.ec2.internal>
2023-02-05 18:56:58 +0000

    adds instance user_data script

---
+++ script_n.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo su -
+sudo apt update -y
+sudo apt install wget
+sudo apt install openjdk-8-jdk
+
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+#echo "export JAVA_HOME=/usr/lib/jvm/java-7-oracle" >>~/.bashrc
+#echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc
+cd /root
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz
+tar -vxzf apache-maven-3.8.7-bin.tar.gz
+export PATH=$PATH:/root/apache-maven-3.8.7-bin/bin
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.tar.gz
+tar -vxzf apache-tomcat-9.0.71.tar.gz
+./root/apache-tomcat-9.0.71/bin/startup.sh
+
